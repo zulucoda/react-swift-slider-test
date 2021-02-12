2ba9a901090cffc5b3e49e284c5e563a00fc4a9e
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2021-02-12 20:15:49 +0200

    react-swift-slider - https://github.com/zulucoda/react-swift-slider/releases/tag/v7.0.0

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import SwiftSlider from 'react-swift-slider'
 
+const data =  [
+  {'id':'1','src':'/assets/images/alfa_romeo_stelvio_01.jpg'},
+  {'id':'2','src':'/assets/images/alfa_romeo_stelvio_02.jpg'},
+  {'id':'3','src':'/assets/images/alfa_romeo_stelvio_03.jpg'},
+  {'id':'4','src':'/assets/images/alfa_romeo_giulia_05.png'},
+  {'id':'5','src':'/assets/images/alfa_romeo_giulia_01.jpg'},
+  {'id':'6','src':'/assets/images/alfa_romeo_giulia_03.png'},
+  {'id':'7','src':'/assets/images/alfa_romeo_giulia_04.png'},
+  {'id':'8','src':'/assets/images/alfa_romeo_giulia_02.png'}
+];
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <SwiftSlider data={data} height={900} />
   );
 }
 
